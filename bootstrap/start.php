ccab8d73251ec0f898500010c6a97a809bbd8562
Владимир <vkharseev@gmail.com>
2014-09-10 19:12:16 +0400

    Направления и курсы

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -11,9 +11,7 @@ $app = new Illuminate\Foundation\Application;
 ########################################################################
 
 $env = $app->detectEnvironment(array(
-	'az' => array('Acer_5742G'),
 	'vkharseev' => array('DNS'),
-	#'production' => array('www.grapheme.ru'),
 ));
 $app->bindInstallPaths(require __DIR__.'/paths.php');
 $framework = $app['path.base'].'/vendor/laravel/framework/src';
