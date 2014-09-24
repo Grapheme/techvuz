b8e9d47596b0cfb57fe42d4ee44a62308863326a
Владимир <vkharseev@gmail.com>
2014-09-24 13:59:45 +0400

    Отзывы

--- app/views/templates/guest/news-list.blade.php
+++ app/views/templates/guest/news-list.blade.php
@@ -1,7 +1,11 @@
+<?php
+    $news_list = News::with('meta.seo')->limit(12)->get();
+?>
+@if($news_list->count())
 <section class="news">
     <h3>Новости</h3>
     <ul class="news-ul clearfix">
-    @foreach(News::with('meta.seo')->limit(12)->get() as $news)
+    @foreach($news_list as $news)
         <li class="news-li">
             <div class="news-date">
                 {{ Helper::rdate("j M Y", strtotime($news->published_at)) }}
@@ -12,3 +16,4 @@
    @endforeach
    </ul>
 </section>
+@endif
