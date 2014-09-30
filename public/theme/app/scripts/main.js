1133a8760b2c1f6fed2410962ff03247bb838fd2
Artem Tumin <arttumin@gmail.com>
2014-09-30 16:22:51 +0400

    cabinet page

--- public/theme/app/scripts/main.js
+++ public/theme/app/scripts/main.js
@@ -1,2 +1,62 @@
 $('.tabs').tabs();
 $('.select').selectmenu();
+
+jQuery.fn.notifications = function() {
+	'use strict';
+
+	var element = $(this),
+		leftBtn = $(this).find('.js-notif-left'),
+		rightBtn = $(this).find('.js-notif-right'),
+		currentFlag = $(this).find('.notifications-count .current'),
+		allFlag = $(this).find('.notifications-count .all'),
+		notifListElem = $(this).find('.notifications-li');
+
+	leftBtn.click( function(){
+		element.trigger('prev-notification');
+	});
+
+	rightBtn.click( function(){
+		element.trigger('next-notification');
+	});
+
+	element.bind('next-notification', function(){
+		var activeElem = notifListElem.filter('.active');
+		var activeElemIndex = activeElem.index() + 1;
+
+		notifListElem.removeClass('active');
+
+		if( activeElemIndex < notifListElem.length ) {
+			activeElem.next().addClass('active');
+		} else {
+			notifListElem.first().addClass('active');
+		}
+		element.trigger('set-count');
+	});
+
+	element.bind('prev-notification', function(){
+		var activeElem = notifListElem.filter('.active');
+		var activeElemIndex = activeElem.index() + 1;
+
+		notifListElem.removeClass('active');
+
+		if( activeElemIndex > 1 ) {
+			activeElem.prev().addClass('active');
+		} else {
+			notifListElem.last().addClass('active');
+		}
+		element.trigger('set-count');
+	});
+
+	element.bind('set-count', function(){
+		allFlag.html( notifListElem.length );
+		currentFlag.html( notifListElem.filter('.active').index() + 1 );
+	});
+
+	//Add active class to the first element of the list
+	notifListElem.first().addClass('active');
+
+	element.trigger('set-count');
+
+};
+
+$('.notifications').notifications();
