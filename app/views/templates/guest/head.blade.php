d804911756cef4ea4a87bae14cfb8c5d51ae8399
Владимир <vkharseev@gmail.com>
2014-09-30 18:10:59 +0400

    Кабинет организации

--- app/views/templates/guest/head.blade.php
+++ app/views/templates/guest/head.blade.php
@@ -13,9 +13,6 @@
 <meta name="description" content="@yield('description')">
 <meta name="keywords" content="@yield('keywords')">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-@if(Config::get('app.use_googlefonts'))
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400,600&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-@endif
 {{ HTML::style(Config::get('site.theme_path').'/styles/vendor.css') }}
 {{ HTML::style(Config::get('site.theme_path').'/styles/main.css') }}
 {{ HTML::script(Config::get('site.theme_path').'/scripts/vendor/modernizr.js') }}
---
+++ app/views/templates/organization.blade.php
@@ -0,0 +1,32 @@
+@if (@is_object($page->meta->seo))
+@section('title'){{ $page->meta->seo->title ? $page->meta->seo->title : $page->name }}@stop
+@section('description'){{ $page->meta->seo->description }}@stop
+@section('keywords'){{ $page->meta->seo->keywords }}@stop
+@elseif (@is_object($page->meta))
+@section('title')
+{{{ $page->name }}}@stop
+@elseif (@is_object($seo))
+@section('title'){{ $seo->title }}@stop
+@section('description'){{ $seo->description }}@stop
+@section('keywords'){{ $seo->keywords }}@stop
+@endif
+<!doctype html>
+<html class="no-js">
+    <head>
+	@include(Helper::layout('head'))
+	@yield('style')
+    </head>
+<body>
+    <!--[if lt IE 10]>
+        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <div class="main-wrapper">
+    @include(Helper::layout('header'))
+    @include(Helper::layout('aside'))
+    @yield('content', @$content)
+    </div>
+    @include(Helper::layout('scripts'))
+    @yield('overlays')
+    @yield('scripts')
+</body>
+</html>
