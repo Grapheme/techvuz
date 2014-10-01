a9937cea0689ac355a425b553f0a4e8e7f76bb99
Владимир <vkharseev@gmail.com>
2014-10-01 15:48:27 +0400

    Сброс пароля. письмо

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -7,7 +7,6 @@
 		<h2>Сброс пароля</h2>
 		<div>
 			Чтобы сбросить пароль, заполните эту <a href="{{ URL::route('password-reset.show', array($token)) }}">форму</a>.<br/>
-			Эта ссылка истекает через {{ round(Config::get('auth.reminder.expire', 60)/1440) }} дней.
 		</div>
 	</body>
 </html>
