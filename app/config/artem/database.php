0cfb32ec177111c1ffa3d10299055620599190b3
Artem Tumin <arttumin@gmail.com>
2014-10-03 14:27:15 +0400

    changes

--- app/config/artem/database.php
+++ app/config/artem/database.php
@@ -1,12 +1,12 @@
 <?php
 
 return array(
-
 	'connections' => array(
 		'mysql' => array(
+			'host'      => 'localhost',
 			'database'  => 'techvuz',
 			'username'  => 'root',
-			'password'  => 'perl03',
+			'password'  => 'perl04',
 		),
-	),
+	)
 );
