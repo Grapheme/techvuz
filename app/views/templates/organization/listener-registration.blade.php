60e0c5d7b8390d8fdabb0bfb7fa0fd794df9c92a
Владимир <vkharseev@gmail.com>
2014-10-03 14:55:49 +0400

    регистрация сотрудника

--- app/views/templates/organization/listener-registration.blade.php
+++ app/views/templates/organization/listener-registration.blade.php
@@ -1,10 +1,45 @@
 @extends(Helper::layout())
 @section('style')
-
 @stop
 @section('content')
-<main class="registration">
-
+<main>
+    <div class="edit-employee-anket">
+        <h3 class="margin-bottom-30">Добавить сотрудника</h3>
+        <form class="registration-form">
+            <div class="form-element">
+                <label>Ф.И.О.</label><input type="text">
+            </div>
+            <div class="form-element">
+                <label>Должность</label><input type="text">
+            </div>
+            <div class="form-element">
+                <label>Email</label><input type="text">
+            </div>
+            <div class="form-element">
+                <label>Адрес</label><input type="text">
+            </div>
+            <div class="form-element">
+                <label>Телефон</label><input type="text">
+            </div>
+            <div class="form-element">
+                <label>Образование</label><input type="text">
+            </div>
+            <div class="form-element">
+                <label>Место работы</label><input type="text">
+            </div>
+            <div class="form-element">
+                <label>Год обучения</label><input type="text">
+            </div>
+            <div class="form-element">
+                <label>Специальность</label><input type="text">
+            </div>
+            <div class="form-element">
+                <button class="btn btn--bordered btn--blue">
+                    Добавить
+                </button>
+            </div>
+        </form>
+    </div>
 </main>
 @stop
 @section('overlays')
