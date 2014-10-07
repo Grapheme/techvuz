80857dc1e455e2bf30d1d7a56102995333947de8
Artem Tumin <arttumin@gmail.com>
2014-10-07 15:51:24 +0400

    cabinet tabs styling

--- app/views/templates/organization/menu.blade.php
+++ app/views/templates/organization/menu.blade.php
@@ -1,4 +1,4 @@
-<ul>
+<ul class="clearfix margin-top-20 margin-bottom-40">
     <li><a href="{{ URL::route('company-orders') }}"><span class="icon icon-zakaz"></span> Заказы</a></li>
     <li><a href="{{ URL::route('company-listeners') }}"><span class="icon icon-slysh"></span> Сотрудники</a></li>
     <li><a href="{{ URL::route('company-study') }}"><span class="icon icon-obych"></span> Обучение</a></li>
