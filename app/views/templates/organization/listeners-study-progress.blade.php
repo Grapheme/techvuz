d56a9aceaa00b9d9d5216371d2cc7b9cd089f9eb
Владимир <vkharseev@gmail.com>
2014-10-07 16:03:52 +0400

    Процес обучения

--- app/views/templates/organization/listeners-study-progress.blade.php
+++ app/views/templates/organization/listeners-study-progress.blade.php
@@ -5,23 +5,28 @@
 
 <main class="cabinet">
     <?php
-
-    $listeners = User_listener::where('organization_id',Auth::user()->id)->where('active',1)->orderBy('created_at','DESC')->with(array('study'=>function($query){
+    $listeners = User_listener::where('organization_id',Auth::user()->id)->where('active',1)->with(array('study'=>function($query){
         $query->where('start_status',1);
         $query->where('over_status',0);
         $query->orderBy('start_date','DESC');
         $query->with('order');
         $query->with('course');
     }))->get();
+
+    $hasStudyProgress = FALSE;
+    foreach($listeners as $listener):
+        if($listener->study->count()):
+            $hasStudyProgress = TRUE;
+            break;
+        endif;
+    endforeach;
     ?>
-
-{{--    {{ Helper::tad($listeners); }}--}}
-
     <h2>{{ User_organization::where('id',Auth::user()->id)->first()->title }}</h2>
     <div class="cabinet-tabs">
         @include(Helper::acclayout('menu'))
         <div class="employees margin-bottom-40">
             <h3 class="margin-bottom-20">Ход обучения</h3>
+        @if($hasStudyProgress)
             <table class="tech-table sortable">
                 <tbody>
                     <tr>
@@ -58,6 +63,11 @@
                 @endforeach
                 </tbody>
             </table>
+        @else
+            <div class="banner banner--red">
+                <span>На данный момент никто не обучается</span>
+            </div>
+        @endif
         </div>
     </div>
 </main>
