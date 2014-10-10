7acfc83281b7a4d081df5c223f12d95cdbcd47a4
Владимир <vkharseev@gmail.com>
2014-10-10 18:30:26 +0400

    Профиль компании!Отладка

--- app/views/templates/moderator/company-profile.blade.php
+++ app/views/templates/moderator/company-profile.blade.php
@@ -139,7 +139,7 @@
     <div class="count-add">
         <?php $activeListenersIDs = array(); ?>
         <?php $allListenersIDs = array(); ?>
-        {{ Helper::dd($orders); }}
+        {{ Helper::tad($orders); }}
         @foreach($orders as $order)
             @if(in_array($order->payment_status,array(2,3)) && $order->close_status == 0)
                 @foreach($order->listeners as $listener)
