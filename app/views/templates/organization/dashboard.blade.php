17d1ddca2cb7d3a6b05f22f03c7900186fa9e968
Artem Tumin <arttumin@gmail.com>
2014-11-27 17:23:47 +0300

    ben noti list

--- app/views/templates/organization/dashboard.blade.php
+++ app/views/templates/organization/dashboard.blade.php
@@ -115,7 +115,7 @@
                                 <div class="notif-delete js-notif-delete">
                                 {{ Form::open(array('url'=>URL::route('organization-notification-delete',array('notification_id'=>$message->id)), 'style'=>'display:inline-block', 'method'=>'delete')) }}
                                     
-                                    <button type="submit" class="btn btn--bordered btn--danger">Удалить</button>
+                                    <button type="submit" class="delete-btn">Удалить</button>
 
                                 {{ Form::close() }}
                                 </div>
