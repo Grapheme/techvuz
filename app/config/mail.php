ed19ee0cfa6949a326cb2da5a30fb8e46b2a4135
Владимир <vkharseev@gmail.com>
2014-11-27 11:38:49 +0300

    mail config

--- app/config/mail.php
+++ app/config/mail.php
@@ -6,15 +6,25 @@ return array(
         'address' => 'support@grapheme.ru',
     ),
 
-    'driver' => 'smtp',
-    'host' => 'smtp.yandex.ru',
+    'driver' => 'sendmail',
+    'host' => '',
     'port' => 465,
-    'from' => array('address' => 'dah-sl@yandex.ru', 'name' => 'ТехВуз.рф'),
-    'username' => 'dah-sl@yandex.ru',
-    'password' => '534msd8HHS',
+    'from' => array('address' => '', 'name' => 'ТехВуз.рф'),
+    'username' => '',
+    'password' => '',
 
     'sendmail' => '/usr/sbin/sendmail -bs',
     'encryption' => 'ssl',
 
     'pretend' => FALSE
 );
+
+//'driver' => 'smtp',
+//'host' => 'smtp.yandex.ru',
+//'port' => 465,
+//'from' => array('address' => 'dah-sl@yandex.ru', 'name' => 'ТехВуз.рф'),
+//'username' => 'dah-sl@yandex.ru',
+//'password' => '534msd8HHS',
+//
+//'sendmail' => '/usr/sbin/sendmail -bs',
+//'encryption' => 'ssl',
