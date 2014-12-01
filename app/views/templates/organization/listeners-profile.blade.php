ba568e2c552a9e96461bf809eb6c2b284bf28afd
Artem Tumin <arttumin@gmail.com>
2014-12-01 15:48:39 +0300

    +++

--- app/views/templates/organization/listeners-profile.blade.php
+++ app/views/templates/organization/listeners-profile.blade.php
@@ -79,7 +79,7 @@
         </table>
         @endif
         <div class="employer-anket margin-top-20 margin-bottom-40">
-            <div class="row margin-bottom-30">
+            <div class="row margin-bottom-10">
                 <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
                     <h3>Анкета сотрудника</h3>
                 </div>
