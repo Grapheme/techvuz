1e5c566d2c30243df5039f2d48bcf370e51d0d3b
Artem Tumin <arttumin@gmail.com>
2014-12-01 19:07:50 +0300

    testing

--- app/views/templates/listener/study-test.blade.php
+++ app/views/templates/listener/study-test.blade.php
@@ -40,12 +40,12 @@
             @endforeach
                     <li data-question="finish">
                         <h4>Результаты тестирования</h4>
-                        <div>
+                        <div class="margin-top-20 margin-bottom-20">
                             Вы прошли тестирование, ваш результат:
                             <br> {{ $test->questions->count() }} из {{ $test->questions->count() }}
                         </div>
                         <div>
-                            {{ Form::submit('Просмотреть результат') }}
+                            <button type="submit" class="btn btn--bordered btn--blue">Просмотреть результат</button>
                         </div>
                     </li>
                 </ul>
