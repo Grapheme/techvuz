0b25bae56bd47c82fa2a092a3f63388ab370d305
Artem Tumin <arttumin@gmail.com>
2014-12-02 19:59:33 +0300

    +++

--- public/theme/js/index.js
+++ public/theme/js/index.js
@@ -411,7 +411,7 @@ var Popup = (function(){
 
     function returnError(text) {
     	$('p.error').remove();
-		$('.purchase-course-dl').append('<p class="error" style="position: relative; top: -2rem; height: 0; font-size: 14px; color: #bb252d; font-weight: 400;">' + text + '</p>');
+		$('.purchase-course-dl').append('<p class="error" style="position: relative; top: -1rem; height: 0; font-size: 14px; color: #bb252d; font-weight: 400;">' + text + '</p>');
 
 		setTimeout( function(){ $('p.error').remove(); }, 3000 );
 	}
