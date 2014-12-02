189dd3a5ed724fc2d3f54a3e66042e3022d9386a
Владимир <vkharseev@gmail.com>
2014-12-02 19:21:44 +0300

    Архив обучения

--- app/views/templates/organization/listeners-study-progress.blade.php
+++ app/views/templates/organization/listeners-study-progress.blade.php
@@ -51,6 +51,46 @@
             </p>
         @endif
         </div>
+        <?php
+        $listeners = User_listener::where('organization_id',Auth::user()->id)->where('active','>=',1)->with(array('study'=>function($query){
+            $query->where('start_status',1);
+            $query->where('over_status',1);
+            $query->orderBy('start_date','DESC');
+            $query->with('order');
+            $query->with('course');
+            $query->with('final_test');
+        }))->get();
+
+        $hasStudyProgress = FALSE;
+        foreach($listeners as $listener):
+            if($listener->study->count()):
+                $hasStudyProgress = TRUE;
+                break;
+            endif;
+        endforeach;
+        ?>
+        <div class="employees margin-bottom-40">
+            <h3 class="margin-bottom-20">Архив обучения</h3>
+        @if($hasStudyProgress)
+            <table class="tech-table sortable">
+                <tbody>
+                    <tr>
+                        <th class="sort sort--asc">Ф.И.О. <span class="sort--icon"></span> </th>
+                        <th class="sort sort--asc">Название курса <span class="sort--icon"></span> </th>
+                        <th class="sort sort--asc">Прогресс <span class="sort--icon"></span> </th>
+                    </tr>
+                @foreach($listeners as $listener)
+
+                    @if($listener->study->count())
+                        @foreach($listener->study as $index => $study)
+                            @include(Helper::acclayout('assets.listener-course-tr'))
+                        @endforeach
+                    @endif
+                @endforeach
+                </tbody>
+            </table>
+        @endif
+        </div>
     </div>
 </main>
 @stop
