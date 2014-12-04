db3a66fecbe9eb78e2a8adcd925824550d0a0563
Artem Tumin <arttumin@gmail.com>
2014-12-04 17:34:26 +0300

    favicon

--- app/views/templates/guest/head.blade.php
+++ app/views/templates/guest/head.blade.php
@@ -14,6 +14,9 @@
 <meta name="keywords" content="@yield('keywords')">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400,600&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+<link rel="icon" 
+      type="image/png" 
+      href="{{ HTML::style(Config::get('site.theme_path').'/favicon.png') }}">
 @if(File::exists(public_path('css/vendor.css')))
 {{ HTML::style(Config::get('site.theme_path').'/css/vendor.css') }}
 @endif
