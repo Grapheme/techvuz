9af21dcf0650076e82dc8f40b7e222112d08396b
Artem Tumin <arttumin@gmail.com>
2014-12-04 14:14:11 +0300

    right anchor for courses on index

--- app/views/templates/guest/index.blade.php
+++ app/views/templates/guest/index.blade.php
@@ -15,7 +15,7 @@
             @if(!empty($direction->photo->name))
             {{ HTML::image(Config::get('site.galleries_photo_public_dir').'/'.$direction->photo->name,$direction->title,array('class'=>'directions-img')) }}
             @endif
-                <a class="direction-link" href="{{ URL::route('page','catalog') }}#{{ 2 * $key + 1 }}"></a>
+                <a class="direction-link" href="{{ URL::route('page','catalog') }}#ui-id-{{ 2 * $key + 1 }}"></a>
                 <div class="direction-name">
                     {{ $direction->title }}
                 </div>
