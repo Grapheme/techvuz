535a36305a1373dbe53474bd3ba8a9f273cd715d
Artem Tumin <arttumin@gmail.com>
2014-12-09 14:35:30 +0300

    moder company profile ubiraem govnishe

--- app/views/templates/moderator/company-profile.blade.php
+++ app/views/templates/moderator/company-profile.blade.php
@@ -3,230 +3,234 @@
 @stop
 @section('content')
 <h2 class="margin-bottom-40">{{ $profile->title }}</h2>
-<div class="row">
-    <?php $accountStatus = array('Не активный','Активный','Не активирован')?>
-    @if(isset($accountStatus[$profile->active]))
-        @if($profile->active == 2)
-        <?php $activation_date = '. До '.date("d.m.Y H:i:s",User::where('id',$profile->id)->pluck('code_life'));?>
-        @else
-        <?php $activation_date = ''; ?>
-        @endif
-        <h3>Профиль</h3>
-        <div>
-            {{ $accountStatus[$profile->active] }}{{ $activation_date }}
-        </div>
-        <div>
-            Модератором {{ $profile->moderator_approve ? 'подтвержден' : 'не подтвержден' }}.
-        </div>
-    @endif
-    <div class="employer-anket margin-bottom-40">
-        <div class="row">
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                <a class="icon--blue pull-right margin-top-30" href="{{ URL::route('moderator-company-profile-edit',$profile->id) }}">
-                    <span class="icon icon-red"></span>
-                </a>
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Полное наименование организации</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->title }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">ФИО подписанта договора</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->fio_manager }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">ФИО подписанта договора в род. падеже</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->fio_manager_rod }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Должность подписанта договора</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->manager }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Документ, на основании которого действует подписант</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->statutory }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Юридический адрес</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->uraddress }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Почтовый адрес</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->postaddress }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">ОГРН</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->ogrn }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">ИНН</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->inn }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">КПП</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->kpp }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Расчетный счет</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->account_number }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Корреспондентский счет</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->account_kor_number }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Наименование банка</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->bank }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">БИК</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->bik }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">E-mail</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->email }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Контактное лицо</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->name }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Номер телефона</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->phone }}
-            </div>
-        </div>
-        <div class="row margin-bottom-10">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <span class="font-sm">Скидка</span>
-            </div>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                {{ $profile->discount }}%
-            </div>
-        </div>
-    </div>
-@if($listeners->count())
-    <h3>Сотрудники</h3>
-    <div class="count-add">
-        <?php $activeListenersIDs = array(); ?>
-        @foreach($orders as $order)
-            @if($order->close_status == 0)
-                @foreach($order->listeners as $listener)
-                    @if($listener->start_status == 1)
-                    <?php $activeListenersIDs[$listener->user_id] = 1; ?>
-                    @endif
-                @endforeach
+
+<div class="container-fluid">
+    <div class="row">
+        <?php $accountStatus = array('Не активный','Активный','Не активирован')?>
+        @if(isset($accountStatus[$profile->active]))
+            @if($profile->active == 2)
+            <?php $activation_date = '. До '.date("d.m.Y H:i:s",User::where('id',$profile->id)->pluck('code_life'));?>
+            @else
+            <?php $activation_date = ''; ?>
             @endif
-        @endforeach
+            <h3>Профиль</h3>
+            <div>
+                {{ $accountStatus[$profile->active] }}{{ $activation_date }}
+            </div>
+            <div>
+                Модератором {{ $profile->moderator_approve ? 'подтвержден' : 'не подтвержден' }}.
+            </div>
+        @endif
+        <div class="employer-anket margin-bottom-40">
+            <div class="row">
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    <a class="icon--blue pull-right margin-top-30" href="{{ URL::route('moderator-company-profile-edit',$profile->id) }}">
+                        <span class="icon icon-red"></span>
+                    </a>
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Полное наименование организации</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->title }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">ФИО подписанта договора</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->fio_manager }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">ФИО подписанта договора в род. падеже</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->fio_manager_rod }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Должность подписанта договора</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->manager }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Документ, на основании которого действует подписант</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->statutory }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Юридический адрес</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->uraddress }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Почтовый адрес</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->postaddress }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">ОГРН</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->ogrn }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">ИНН</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->inn }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">КПП</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->kpp }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Расчетный счет</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->account_number }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Корреспондентский счет</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->account_kor_number }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Наименование банка</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->bank }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">БИК</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->bik }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">E-mail</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->email }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Контактное лицо</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->name }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Номер телефона</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->phone }}
+                </div>
+            </div>
+            <div class="row margin-bottom-10">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <span class="font-sm">Скидка</span>
+                </div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                    {{ $profile->discount }}%
+                </div>
+            </div>
+        </div>
+    @if($listeners->count())
+        <h3>Сотрудники</h3>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 no-gutter">
-                    <div class="count-add-sign">Обучается</div>
-                    <div class="count-add-num">{{ count($activeListenersIDs) }}</div>
-                    <div class="count-add-dots"></div>
-                </div>
-                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                    <div class="count-add-sign">Всего</div>
-                    <div class="count-add-num">{{ Listener::where('organization_id',$profile->id)->count() }}</div>
-                    <div class="count-add-dots"></div>
+                <table class="tech-table sortable">
+                    <tbody>
+                        <tr>
+                            <th class="sort sort--asc">Ф.И.О. <span class="sort--icon"></span> </th>
+                            <th class="sort sort--asc">Контакты <span class="sort--icon"></span> </th>
+                        </tr>
+                    @foreach($listeners as $listener)
+                        <tr>
+                            <td>
+                                <a href="{{ URL::route('moderator-listener-profile',$listener->id) }}">{{ $listener->fio }}</a><br>
+                                {{ $listener->position }}<br>
+                                рег.от: {{ $listener->created_at->timezone(Config::get('site.time_zone'))->format("d.m.Y") }}
+                            </td>
+                            <td>
+                                {{ $listener->email }}<br>
+                                {{ $listener->phone }}
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="count-add margin-top-40">
+            <?php $activeListenersIDs = array(); ?>
+            @foreach($orders as $order)
+                @if($order->close_status == 0)
+                    @foreach($order->listeners as $listener)
+                        @if($listener->start_status == 1)
+                        <?php $activeListenersIDs[$listener->user_id] = 1; ?>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-offset-8 col-sm-offset-8 col-md-offset-8 col-lg-offset-8 col-xs-4 col-sm-4 col-md-4 col-lg-4 no-gutter">
+                        <div class="count-add-sign">Обучается</div>
+                        <div class="count-add-num">{{ count($activeListenersIDs) }}</div>
+                        <div class="count-add-dots"></div>
+                    </div>
+                    <div class="col-xs-offset-8 col-sm-offset-8 col-md-offset-8 col-lg-offset-8 col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                        <div class="count-add-sign">Всего</div>
+                        <div class="count-add-num">{{ Listener::where('organization_id',$profile->id)->count() }}</div>
+                        <div class="count-add-dots"></div>
+                    </div>
                 </div>
             </div>
         </div>
+    @endif
     </div>
-    <div class="container-fluid">
-        <div class="row">
-            <table class="tech-table sortable">
-                <tbody>
-                    <tr>
-                        <th class="sort sort--asc">Ф.И.О. <span class="sort--icon"></span> </th>
-                        <th class="sort sort--asc">Контакты <span class="sort--icon"></span> </th>
-                    </tr>
-                @foreach($listeners as $listener)
-                    <tr>
-                        <td>
-                            <a href="{{ URL::route('moderator-listener-profile',$listener->id) }}">{{ $listener->fio }}</a><br>
-                            {{ $listener->position }}<br>
-                            рег.от: {{ $listener->created_at->timezone(Config::get('site.time_zone'))->format("d.m.Y") }}
-                        </td>
-                        <td>
-                            {{ $listener->email }}<br>
-                            {{ $listener->phone }}
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-@endif
 </div>
+
 @stop
 @section('overlays')
 @stop
