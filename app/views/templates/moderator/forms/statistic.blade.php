13988d585796dce3c3207ff28b4c2149f4c006d3
Artem Tumin <arttumin@gmail.com>
2014-12-09 15:19:16 +0300

    statistics page refis

--- app/views/templates/moderator/forms/statistic.blade.php
+++ app/views/templates/moderator/forms/statistic.blade.php
@@ -16,7 +16,7 @@
                 </section>
             </fieldset>
             <footer>
-                <button type="submit" autocomplete="off" class="btn btn-success no-margin margin-top-40 regular-10 uppercase btn-form-submit">
+                <button type="submit" autocomplete="off" class="btn btn-success no-margin regular-10 uppercase btn-form-submit">
                     <i class="fa fa-spinner fa-spin hidden"></i> <span class="btn-response-text">Показать</span>
                 </button>
             </footer>
