aaec2182c8dd0cc5da14c71165ba862bddf29c8c
Владимир <vkharseev@gmail.com>
2014-12-10 14:46:20 +0300

    Аттестационная ведомость

--- app/views/templates/moderator/assets/documents.blade.php
+++ app/views/templates/moderator/assets/documents.blade.php
@@ -1,4 +1,4 @@
-<ul class="margin-top-20">
+<ul class="moder-docs-ul">
     <li><a href="{{ URL::route('moderator-order-contract',array('order_id'=>$order->id,'format'=>'pdf')) }}">Договор</a></li>
     <li><a href="{{ URL::route('moderator-order-invoice',array('order_id'=>$order->id,'format'=>'pdf')) }}">Счет</a></li>
     <li><a href="{{ URL::route('moderator-order-act',array('order_id'=>$order->id,'format'=>'pdf')) }}">Акт</a></li>
