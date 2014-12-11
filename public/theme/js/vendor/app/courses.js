858d263ee722f0583e1edcfcc695584c1c74c6d6
Artem Tumin <arttumin@gmail.com>
2014-12-11 14:25:22 +0300

    ++

--- public/theme/js/vendor/app/courses.js
+++ public/theme/js/vendor/app/courses.js
@@ -33,7 +33,7 @@ var Courses = (function(){
 		var parsedCookie = JSON.parse( $.cookie('ordering') );
 
 		$checked.each( function(){
-			if( parsedCookie[ $(this).val() ] !== [] ) {
+			if( parsedCookie[ $(this).val() ] != [] ) {
 				renderArr[ $(this).val() ] = parsedCookie[ $(this).val() ];
 			} else {
 				renderArr[ $(this).val() ] = [];
