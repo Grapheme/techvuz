a21042da2e050e21eec45eeb39cee2e716328c30
Artem Tumin <arttumin@gmail.com>
2014-12-15 16:01:49 +0300

    listeners reg

--- app/views/templates/organization/listener-registration.blade.php
+++ app/views/templates/organization/listener-registration.blade.php
@@ -27,6 +27,18 @@
      $(document).ready(function(){
          $(".phone").inputmask("mask", {"mask": "[+7] (999) 999 99 99","placeholder": "X"});
          $(".year").inputmask("mask", {"mask": "9999","placeholder": "X"});
+
+         $('.listener-add-form input[name="email"]').keyup( function(e){
+            var key = event.keyCode || event.charCode;
+            if( key == 8 || key == 46 || key == 37 || key == 38 || key == 39 || key == 40)
+                return false;
+
+            this.value = this.value.replace(/[а-яА-яЁё]/i, "");
+         });
+
+         $('.listener-add-form input[name="fio"], .listener-add-form input[name="fio_dat"], .listener-add-form input[name="position"], .listener-add-form input[name="education"]').keyup(function(e){
+            this.value = this.value.replace(/[0123456789]/i, "");
+         });
      });
  </script>
 @stop
