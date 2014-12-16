e2e9116673b439ee77acb93e4f9b555be1b73e4a
Artem Tumin <arttumin@gmail.com>
2014-12-16 16:48:53 +0300

    return

--- public/theme/js/index.js
+++ public/theme/js/index.js
@@ -707,73 +707,73 @@ $('.notifications').notifications();
 
 // Place any jQuery/helper plugins in here.
 
-// var Courses = (function(){
-// 	var $parent = $('.accordion');
-// 	var $secondaryCheckbox = $parent.find('.secondary-checkbox');
-// 	var $mainCheckbox = $parent.find('.main-checkbox');
-// 	var $accBtn = $('.js-btn-acc');
+var Courses = (function(){
+	var $parent = $('.accordion');
+	var $secondaryCheckbox = $parent.find('.secondary-checkbox');
+	var $mainCheckbox = $parent.find('.main-checkbox');
+	var $accBtn = $('.js-btn-acc');
 
-// 	//Загружаем чекнутые боксы on document ready
-// 	$( function(){
-// 		var ordering = $.cookie('ordering') ? JSON.parse( $.cookie('ordering').split(',') ) : '';
+	//Загружаем чекнутые боксы on document ready
+	$( function(){
+		var ordering = $.cookie('ordering') ? JSON.parse( $.cookie('ordering').split(',') ) : '';
 
-// 		//Сбрасываем чекбоксы, которые запомнил браузер
-// 		$secondaryCheckbox.prop('checked', false);
+		//Сбрасываем чекбоксы, которые запомнил браузер
+		$secondaryCheckbox.prop('checked', false);
 
-// 		for (var key in ordering) {
-// 			$secondaryCheckbox.filter('[value="' + key + '"]').prop('checked', true);
-// 		}
+		for (var key in ordering) {
+			$secondaryCheckbox.filter('[value="' + key + '"]').prop('checked', true);
+		}
 
-// 		//Если есть чекнутые чекбоксы - показываем кнопку "Далее"
-// 		if( jQuery.isEmptyObject( ordering ) || !ordering ) {
-// 			$accBtn.hide();
-// 		} else {
-// 			$accBtn.show();
-// 		}
+		//Если есть чекнутые чекбоксы - показываем кнопку "Далее"
+		if( jQuery.isEmptyObject( ordering ) || !ordering ) {
+			$accBtn.hide();
+		} else {
+			$accBtn.show();
+		}
 
-// 	});
+	});
 
-// 	//для каждого клика на чекбокс мы должны обновлять массив заказанных курсов
+	//для каждого клика на чекбокс мы должны обновлять массив заказанных курсов
 
-// 	function renderBuyers(){
-// 		var $parent = $('.accordion-form');
-// 		var $checked = $parent.find('.secondary-checkbox:checked');
-// 		var renderArr = {};
-// 		var existsCookie = $.cookie('ordering') ? JSON.parse( $.cookie('ordering') ) : {};
+	function renderBuyers(){
+		var $parent = $('.accordion-form');
+		var $checked = $parent.find('.secondary-checkbox:checked');
+		var renderArr = {};
+		var existsCookie = $.cookie('ordering') ? JSON.parse( $.cookie('ordering') ) : {};
 
-// 		$checked.each( function(){
-// 			if( existsCookie[ $(this).val() ] && existsCookie[ $(this).val() ].length > 0 ) {
-// 				renderArr[ $(this).val() ] = existsCookie[ $(this).val() ];
-// 			} else {
-// 				renderArr[ $(this).val() ] = [];
-// 			}
-// 		});
+		$checked.each( function(){
+			if( existsCookie[ $(this).val() ] && existsCookie[ $(this).val() ].length > 0 ) {
+				renderArr[ $(this).val() ] = existsCookie[ $(this).val() ];
+			} else {
+				renderArr[ $(this).val() ] = [];
+			}
+		});
 
-// 		$.cookie('ordering', JSON.stringify(renderArr), { path: '/' });
+		$.cookie('ordering', JSON.stringify(renderArr), { path: '/' });
 
-// 		if( jQuery.isEmptyObject( renderArr ) || !renderArr ) {
-// 			$accBtn.hide();
-// 		} else {
-// 			$accBtn.show();
-// 		}
-// 	}
+		if( jQuery.isEmptyObject( renderArr ) || !renderArr ) {
+			$accBtn.hide();
+		} else {
+			$accBtn.show();
+		}
+	}
 
-// 	//События, которые срабатывают при клике на чекбокс
+	//События, которые срабатывают при клике на чекбокс
 	
-// 	$mainCheckbox.on('change', function(){
-// 		if( $(this).prop('checked') ) {
-// 			$(this).parents('.accordion-body').find('.secondary-checkbox').prop('checked', true);
-// 		} else {
-// 			$(this).parents('.accordion-body').find('.secondary-checkbox').prop('checked', false);
-// 		}
-// 		renderBuyers();
-// 	});
+	$mainCheckbox.on('change', function(){
+		if( $(this).prop('checked') ) {
+			$(this).parents('.accordion-body').find('.secondary-checkbox').prop('checked', true);
+		} else {
+			$(this).parents('.accordion-body').find('.secondary-checkbox').prop('checked', false);
+		}
+		renderBuyers();
+	});
 
-// 	$secondaryCheckbox.on('change', function(){
-// 		renderBuyers();
-// 	});
+	$secondaryCheckbox.on('change', function(){
+		renderBuyers();
+	});
 
-// })();
+})();
 var Payments = (function(){
 	var $deletePayment = $('.js-delete-payment');
 	var $editPayment = $('.js-edit-payment');
