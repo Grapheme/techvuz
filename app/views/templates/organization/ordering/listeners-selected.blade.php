88087357d7b4789d2bd9f387625fa33c85c96921
Artem Tumin <arttumin@gmail.com>
2014-12-17 12:41:00 +0300

    Blue discount

--- app/views/templates/organization/ordering/listeners-selected.blade.php
+++ app/views/templates/organization/ordering/listeners-selected.blade.php
@@ -93,7 +93,7 @@
                         </div>
                     </div> 
                     <div class="row no-gutter margin-bottom-20 hidden">
-                        <div class="col-xs-offset-6 col-sm-offset-6 col-md-offset-6 col-lg-offset-6 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <div class="col-xs-offset-6 col-sm-offset-6 col-md-offset-6 col-lg-offset-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 icon--blue">
                             <div class="count-add-sign">Скидка</div>
                             <div class="count-add-num js-count-finish-discount"></div>
                             <div class="count-add-dots"></div>
