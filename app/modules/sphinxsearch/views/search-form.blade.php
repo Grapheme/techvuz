4a0e36282464241f2bf8c512d8958c50bbbfa3b7
Владимир <vkharseev@gmail.com>
2014-12-18 19:26:21 +0300

    no-reply@tehvuz.ru

--- app/modules/sphinxsearch/views/search-form.blade.php
+++ app/modules/sphinxsearch/views/search-form.blade.php
@@ -1,4 +1,4 @@
-{{ Form::open(array('url'=>URL::route('search_request'),'method'=>'post','class'=>'main-search')) }}
+{{ Form::open(array('url'=>URL::route('search_request'),'method'=>'post','class'=>'main-search','style'=>'visibility:hidden')) }}
 <fieldset>
     <input type="text" name="search_request" class="search-input" value="{{ Input::get('query') }}">
     <button type="submit"><span class="icon icon-search"></span></button>
