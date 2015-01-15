15b452fc200f7aba7e67229c9d96002ff233b18c
Toomean <arttumin@gmail.com>
2015-01-15 16:43:21 +0300

    404 in progress

--- app/views/error404.blade.php
+++ app/views/error404.blade.php
@@ -3,9 +3,52 @@
 <head>
 <meta charset="utf-8">
 <title>Страница не найдена :(</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400,600&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+{{ HTML::style(Config::get('site.theme_path').'/css/main.css') }}
 <style>
+	html, body {
+    	margin: 0;
+    	padding: 0;
+   		height: 100%; 
+    }
+  	body {
+    	background: #33ace4;
+    	color: #fff;
+    }
+    .aside-404 {
+   		position: absolute;
+    	top: 0;
+    	left: 0;
+    
+    	width: 250px;
+    	height: 100%;
+    }
+  	.container-404 {
+    	padding: 0 0 0 250px;
+  	}
+  	.contact {
+		margin: 1rem 0 0 0;
+  	}
+  	.phone {
+		font-size: 1.2em;
+		line-height: 1.9em;
+		font-weight: 400;
+  	}
+  	.phone-desc {
+		font-size: 12em / $hfs;
+		line-height: (14.4em / 12);
+  	}
+  	.logo {
+		position: relative;
+		width: 187px;
+		height: 11.6875rem;
+		margin: 0 0 2.95rem;
 
+		background: url(../img/logo.svg);
+		background-size: 100% 100%;
+  	}
 </style>
+
 </head>
 <body>
 	<aside class="aside-404">
@@ -30,7 +73,7 @@
 		    </div>
 		</footer>
 	</aside>
-	
+
 	<div class="container-404">
 		<h1>
 			404 Ошибка
