78deaf23dadc8375ffd214b074321d0f1f467f8e
Toomean <arttumin@gmail.com>
2015-01-20 14:23:21 +0300

    +

--- public/theme/js/main.js
+++ public/theme/js/main.js
@@ -27,13 +27,12 @@ $('.accordion').accordion({
 	var $mainSelect = $('.moderator-cabinet select[name="course_id"]');
 	var $secSelect = $('.moderator-cabinet select[name="chapter_id"]');
 
-	var $secOptions = $secSelect.find('option').clone();
+	var $secOptions = $secSelect.find('option');
 
 	$mainSelect.change( function(){
 		var currIndex = $(this).find(':selected').attr('value');
-
-		$secSelect.find('option').not('[value="0"]').remove();
-		$secSelect.append($secOptions.filter('[data-course="' + currIndex + '"]'))); 
+		$secSelect.find('option').not('[value="0"]').hide();
+		$secSelect.find('option').filter('[data-course="' + currIndex + '"]').show();
 
 		$secSelect.val( $secSelect.find('option:visible').eq(0).attr('value') ).trigger('change');
 	});
