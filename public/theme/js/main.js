a07465c943fc31cdf2179e84dea6a2f2ed0d60f1
Toomean <arttumin@gmail.com>
2015-01-20 14:33:28 +0300

    hide/show to prepend

--- public/theme/js/main.js
+++ public/theme/js/main.js
@@ -23,23 +23,25 @@ $('.accordion').accordion({
 });
 
 //moderator filter selects
-// (function(){
-// 	var $mainSelect = $('.moderator-cabinet select[name="course_id"]');
-// 	var $secSelect = $('.moderator-cabinet select[name="chapter_id"]');
+(function(){
+	var $mainSelect = $('.moderator-cabinet select[name="course_id"]');
+	var $secSelect = $('.moderator-cabinet select[name="chapter_id"]');
 
-// 	var $secOptions = $secSelect.find('option');
+	var $secOptions = $secSelect.find('option').clone();
 
-// 	$mainSelect.change( function(){
-// 		var currIndex = $(this).find(':selected').attr('value');
-// 		$secSelect.find('option').not('[value="0"]').hide();
-// 		$secSelect.find('option').filter('[data-course="' + currIndex + '"]').show();
+	$mainSelect.change( function(){
+		var currIndex = $(this).find(':selected').attr('value');
+    
+		$secSelect.find('option').not('[value="0"]').remove();
+    
+		$secSelect.prepend($secOptions.filter('[data-course="' + currIndex + '"]'));
 
-// 		$secSelect.val( $secSelect.find('option:visible').eq(0).attr('value') ).trigger('change');
-// 	});
+		$secSelect.val( $secSelect.find('option:visible').eq(0).attr('value') ).trigger('change');
+	});
 
-// 	$mainSelect.change();
+	$mainSelect.change();
 
-// })();
+})();
 
 (function(){
 
