d13892de4e1f363c4bfc7e59b1eb9ff2076e7f86
Toomean <arttumin@gmail.com>
2015-01-26 16:53:46 +0300

    courses in progress

--- app/views/templates/moderator/scripts.blade.php
+++ app/views/templates/moderator/scripts.blade.php
@@ -31,5 +31,9 @@
 <script>
      $(document).ready(function(){
          $(".phone").inputmask("mask", {"mask": "[+7] (999) 999 99 99","placeholder": "X"});
+         $('[data-toggle="tooltip"]').tooltip();
+         $('.course-in-progress > a').click( function(e){
+         	e.preventDefault();
+         });
      });
  </script>
