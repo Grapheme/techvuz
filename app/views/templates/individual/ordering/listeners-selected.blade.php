e610d05633e71538ea5903deb73883b9375b7572
Toomean <arttumin@gmail.com>
2015-01-28 18:20:51 +0300

    listener sums

--- app/views/templates/individual/ordering/listeners-selected.blade.php
+++ app/views/templates/individual/ordering/listeners-selected.blade.php
@@ -83,7 +83,21 @@
                 <div class="container-fluid">
                     <div class="row no-gutter margin-bottom-20">
                         <div class="col-xs-offset-6 col-sm-offset-6 col-md-offset-6 col-lg-offset-6 col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                            <div class="count-add-sign">Итоговая сумма</div>
+                            <div class="count-add-sign">Сумма</div>
+                            <div class="count-add-num"></div>
+                            <div class="count-add-dots"></div>
+                        </div>
+                    </div>
+                    <div class="row no-gutter margin-bottom-20 hidden">
+                        <div class="col-xs-offset-6 col-sm-offset-6 col-md-offset-6 col-lg-offset-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 icon--blue">
+                            <div class="count-add-sign">Скидка</div>
+                            <div class="count-add-num"></div>
+                            <div class="count-add-dots"></div>
+                        </div>
+                    </div> 
+                    <div class="row no-gutter margin-bottom-20">
+                        <div class="col-xs-offset-6 col-sm-offset-6 col-md-offset-6 col-lg-offset-6 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="count-add-sign">К оплате</div>
                             <div class="count-add-num">{{ number_format($totalPrice,0,'.','') }}.–</div>
                             <div class="count-add-dots"></div>
                         </div>
