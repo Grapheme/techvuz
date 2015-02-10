c23b222c977e47fdfda6eb322d575bf53d7d3188
Владимир <vkharseev@gmail.com>
2015-02-10 20:54:48 +0300

    метрика

--- app/views/templates/guest/scripts.blade.php
+++ app/views/templates/guest/scripts.blade.php
@@ -96,5 +96,33 @@
         e.preventDefault();
      });
      $('.accordion-header.direction-in-progress').addClass('ui-state-disabled');
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter28373826 = new Ya.Metrika({
+                    id:28373826,
+                    webvisor:true,
+                    trackHash:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/28373826" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 </script>
 @endif
