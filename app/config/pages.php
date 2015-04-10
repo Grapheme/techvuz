29469dd6d8890a0748d306d87bd2e2ebcf023e47
Владимир <vkharseev@gmail.com>
2015-04-10 12:12:31 +0300

    Новый модуль страницы

--- app/config/pages.php
+++ app/config/pages.php
@@ -11,7 +11,7 @@ return array(
     'disable_url_modification' => true, ## отключить модификаторы урлов. Не включать!
     'disable_slug_to_template' => true, ## отключить автоматический поиск шаблона страницы по ее системному имени в случае, если страница не существует
 
-    'preload_pages_limit' => 0, ## NULL - never; 0 - always; 100 - if less than 100 (+one more sql request)
+    'preload_pages_limit' => NULL, ## NULL - never; 0 - always; 100 - if less than 100 (+one more sql request)
     'preload_cache_lifetime' => 60*24, ## время жизни кеша страниц, в минутах
 
 );
