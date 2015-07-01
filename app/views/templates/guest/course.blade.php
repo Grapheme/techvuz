922d4ac16a70d7618318f73a4cc65f193b28d7ad
Kirill Fadeev <fk@realitygroup.ru>
2015-07-01 14:58:35 +0300

    fix

--- app/views/templates/guest/course.blade.php
+++ app/views/templates/guest/course.blade.php
@@ -17,7 +17,7 @@
     <div class="btn-request-holder">
     @if(Auth::guest())
         <a class="btn-request" href="{{ pageurl('registration') }}">Оформить заявку</a>
-    @else
+    @elseif(isOrganizationORIndividual())
         <div class="new-order-holder">
             <a href="{{ URL::route('ordering-select-courses') }}#{{ $course->id }}" class="btn btn-top-margin btn--bordered btn--blue pull-right">Новый заказ</a>
         </div>
