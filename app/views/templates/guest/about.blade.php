3cf3618bfa7098a9741be22ade37352038feef04
Kirill Fadeev <fk@realitygroup.ru>
2015-07-15 19:18:38 +0300

    fix

--- app/views/templates/guest/about.blade.php
+++ app/views/templates/guest/about.blade.php
@@ -3,9 +3,7 @@
 @section('content')
 <main class="contacts">
     <h1>{{ $page->block('top_h2') }}</h1>
-    <div class="desc">
-    {{ $page->block('top_desc') }}
-    </div>
+
     <?php
         $licenses = Dictionary::whereSlugValues('licenses-certificates');
         $images = array();
@@ -38,6 +36,11 @@
         @endforeach
     </ul>
     @endif
+
+    <div class="desc">
+        {{ $page->block('top_desc') }}
+    </div>
+
     <div class="desc">{{ $page->block('seo') }}</div>
 </main>
 @stop
