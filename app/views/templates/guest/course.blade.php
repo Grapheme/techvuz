46e482e0a291a926ed250493af26a21a9d4dfb6c
Владимир <vkharseev@gmail.com>
2015-09-08 11:49:03 +0300

    Страница учебного плана одного курса — поле SEO-текст нужно перенести на позицию после таблицы

--- app/views/templates/guest/course.blade.php
+++ app/views/templates/guest/course.blade.php
@@ -7,26 +7,32 @@
 @section('style')
 @stop
 @section('content')
-<main class="catalog-study-plan">
-    <p class="study-plan-desc">Учебный план<br>дополнительной профессиональной программы</p>
-    <h1>{{ $course->seo->h1 }}</h1>
-    {{ $course->curriculum }}
-    <div class="desc">
-        {{ $course->description }}
-    </div>
-    <div class="btn-request-holder">
-    @if(Auth::guest())
-        <a class="btn-request" href="{{ pageurl('registration') }}">Оформить заявку</a>
-    @elseif(isOrganizationORIndividual())
-        <div class="new-order-holder">
-            <a href="{{ URL::route('ordering-select-courses') }}#{{ $course->id }}" class="btn btn-top-margin btn--bordered btn--blue pull-right">Новый заказ</a>
+    <main class="catalog-study-plan">
+        <p class="study-plan-desc">Учебный план<br>дополнительной профессиональной программы</p>
+
+        <h1>{{ $course->seo->h1 }}</h1>
+
+        <div class="desc">
+            {{ $course->curriculum }}
         </div>
-    @endif
-    @if(!empty($course->trial_test))
-        <a class="btn-request" href="{{ URL::route('course-page-trial-test', $course->seo->url) }}">{{ $course->trial_test->title }}</a>
-    @endif
-    </div>
-</main>
+        <div class="desc">
+            {{ $course->description }}
+        </div>
+        <div class="btn-request-holder">
+            @if(Auth::guest())
+                <a class="btn-request" href="{{ pageurl('registration') }}">Оформить заявку</a>
+            @elseif(isOrganizationORIndividual())
+                <div class="new-order-holder">
+                    <a href="{{ URL::route('ordering-select-courses') }}#{{ $course->id }}"
+                       class="btn btn-top-margin btn--bordered btn--blue pull-right">Новый заказ</a>
+                </div>
+            @endif
+            @if(!empty($course->trial_test))
+                <a class="btn-request"
+                   href="{{ URL::route('course-page-trial-test', $course->seo->url) }}">{{ $course->trial_test->title }}</a>
+            @endif
+        </div>
+    </main>
 @stop
 @section('overlays')
 @stop
